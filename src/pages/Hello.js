a832581d6c5a326ead762e2acd44c3fb2ec303e5
jiheunkim <ziheun54@gmail.com>
2023-05-25 19:30:48 +0900

    circle

--- src/pages/Hello.js
+++ src/pages/Hello.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components"
 
 const Hello = () => {
   return (
     <div>
       <h1>동그라미</h1>
-      <h1>동그라미</h1>
-      <h1>동그라미</h1>
-      <button title="동그라미" color="#2E8B57" id="btn"></button>
+
+      <circle title="동그라미" color="#2E8B57" id="btn">동그라미</circle>
       <Link to="/">Home</Link>
     </div>
   );
 };
 
-
-
 export default Hello;
+
+const Circle = styled(Rec)`
+  border-radius : 50%;
+`;
