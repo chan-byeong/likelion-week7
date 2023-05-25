3bad5bac919ef2bfffba4d29155fe1c89d90e11f
jiheunkim <ziheun54@gmail.com>
2023-05-25 19:48:56 +0900

    circle

--- src/pages/Hello.js
+++ src/pages/Hello.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components"
 
 const Hello = () => {
   const nav = useNavigate();
   return (
     <div>
-      <h1>동그라미</h1>
-
-      <circle onClick={()=>{nav("/home")}} color="#2E8B57" id="btn">동그라미</circle>
-      <Link to="/">Home</Link>
+      <h1>Hello</h1>
+      <Circle onClick={()=>{nav("/")}}>Home</Circle>
     </div>
   );
 };
 
 export default Hello;
 
-const Circle = styled(Rec)`
+const Circle = styled.div`
   border-radius : 50%;
+  display : flex;
+  align-items : center;
+  justify-content : center;
+  width : 100px;
+  height : 100px;
+  background-color : pink;
+  cursor : pointer;
+  text-align : center;
+  margin : 20px 0;
+  `;
