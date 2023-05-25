01669ad40475f58affa15bb5b91b8bfc4eb7aba8
jiheunkim <ziheun54@gmail.com>
2023-05-25 19:21:14 +0900

    ex

--- src/pages/Hello.js
+++ src/pages/Hello.js
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 const Hello = () => {
   return (
     <div>
-      <h1>Hello</h1>
-      <p>인사 페이지</p>
+      <h1>동그라미</h1>
+      <h1>동그라미</h1>
+      <h1>동그라미</h1>
+      <button title="동그라미" color="#2E8B57" id="btn"></button>
       <Link to="/">Home</Link>
     </div>
   );
 };
+
+
 
 export default Hello;
