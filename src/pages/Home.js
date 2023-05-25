f18c043ea00bd1beb6f5cd9a4bb5909072b119e6
byeong <qudcks4885@gmail.com>
2023-05-25 19:22:14 +0900

    Hompage 버튼 만들기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components"
 
 //about 네모
 //detail 세모
 //hello 동그라미
 
 const Home = () => {
+  
+  const nav = useNavigate();
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>홈페이지</p>
-      <Link to="/about">About</Link>
+    <Main>
+      <h1>Home</h1>    
+
+      <Rec onClick={()=>{nav("/about")}}><div>네모</div></Rec>
+      {/* <Link to="/about">About</Link> */}
       <div></div>
-      <Link to="/detail">Detail</Link>
+      <Tri onClick={()=>{nav("/detail")}}>세모</Tri>
+
+      <Circle onClick={()=>{nav("/hello")}} >동그라미</Circle>
+      {/* <Link to="/detail">Detail</Link> */}
       <div></div>
-      <Link to="/hello">Hello</Link>
-      <div>HI</div>
-    </div>
+      {/* <Link to="/hello">Hello</Link> */}
+    </Main>
   );
 };
 
 export default Home;
+
+const Main = styled.div`
+  width : 500px;
+  height : 500px;
+  display : flex;
+  flex-direction : column;
+  align-items : center;
+  justifiy-content : center;
+`;
+
+const Rec = styled.div`
+  display : flex;
+  align-items : center;
+  justify-content : center;
+  width : 100px;
+  height : 100px;
+  background-color : royalblue;
+  cursor : pointer;
+  text-align : center;
+  margin : 20px 0;
+`;
+
+const Circle = styled(Rec)`
+  border-radius : 50%;
+`;
+
+const Tri = styled.div`
+cursor : pointer;
+width: 0;
+height: 0;
+border-bottom: 50px solid transparent;
+border-top: 50px solid transparent;
+border-left: 50px solid skyblue;
+border-right: 50px solid transparent;
+`;
